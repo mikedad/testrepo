b63b20085a308d12bc3ffef67b646f94995dcbe3
Mike Lemp <mikeiii.email@gmail.com>
2012-05-22 08:02:08 -0700

    a small change to alert.js

--- alert.js
+++ alert.js
@@ -7,3 +7,5 @@ function Begin()
 //Here's a new line
 
 //that's all four files getting a change.  Lets take a look at how this looks in the new windows github app.
+
+//making a change to the alert.js file to see what happens after the previous commit
