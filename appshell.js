b710773164129ee5d516ac9b11ddf065bb14d925
mikedad <mikeiii.email@gmail.com>
2012-05-22 07:54:20 -0700

    I added some lines involving viewModel.mychange directly via the web interface

--- appshell.js
+++ appshell.js
@@ -5,3 +5,8 @@ viewModel.msg = "this is a string";
 
 //[ml] the following items were added on 5/22/2012
 viewModel.reportTitle = "This is the report title";	//fixed on 5/22 at 7:48a
+
+//[ml] here is a change I made directly on the website, so it won't be on my local machine
+//the idea being that this would be similar to the sceneraio
+//where a change made by another user and commited
+viewModel.mychange = "yepper";
