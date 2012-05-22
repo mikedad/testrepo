3d6f6d9ef4dde8b056d6347837d8a3afa066bf22
Mike Lemp <mikeiii.email@gmail.com>
2012-05-22 07:46:01 -0700

    added several lines to the appshell.js file
    not sure if the file was already on my local hd, a little confused at
    the moment, but there were already differences

--- appshell.js
+++ appshell.js
@@ -2,3 +2,6 @@ var viewModel = {};
 
 viewModel.count = 5;
 viewModel.msg = "this is a string";
+
+//[ml] the following items were added on 5/22/2012
+viewModel.reportTitle = "This is the report toitle";
