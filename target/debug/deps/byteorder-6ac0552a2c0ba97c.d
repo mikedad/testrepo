90d44b747b69fac848c400b8e9985b47ffda9094
mikedad <mikeiii.email@gmail.com>
2026-02-13 05:02:36 +0000

    Writing Tests

---
+++ target/debug/deps/byteorder-6ac0552a2c0ba97c.d
@@ -0,0 +1,8 @@
+/worktrees/testrepo/finn/target/debug/deps/byteorder-6ac0552a2c0ba97c.d: /home/vscode/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/byteorder-1.5.0/src/lib.rs /home/vscode/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/byteorder-1.5.0/src/io.rs
+
+/worktrees/testrepo/finn/target/debug/deps/libbyteorder-6ac0552a2c0ba97c.rlib: /home/vscode/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/byteorder-1.5.0/src/lib.rs /home/vscode/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/byteorder-1.5.0/src/io.rs
+
+/worktrees/testrepo/finn/target/debug/deps/libbyteorder-6ac0552a2c0ba97c.rmeta: /home/vscode/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/byteorder-1.5.0/src/lib.rs /home/vscode/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/byteorder-1.5.0/src/io.rs
+
+/home/vscode/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/byteorder-1.5.0/src/lib.rs:
+/home/vscode/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/byteorder-1.5.0/src/io.rs:
---
+++ target/debug/deps/color_quant-de39834b48168b84.d
@@ -0,0 +1,8 @@
+/worktrees/testrepo/finn/target/debug/deps/color_quant-de39834b48168b84.d: /home/vscode/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/color_quant-1.1.0/src/lib.rs /home/vscode/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/color_quant-1.1.0/src/math.rs
+
+/worktrees/testrepo/finn/target/debug/deps/libcolor_quant-de39834b48168b84.rlib: /home/vscode/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/color_quant-1.1.0/src/lib.rs /home/vscode/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/color_quant-1.1.0/src/math.rs
+
+/worktrees/testrepo/finn/target/debug/deps/libcolor_quant-de39834b48168b84.rmeta: /home/vscode/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/color_quant-1.1.0/src/lib.rs /home/vscode/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/color_quant-1.1.0/src/math.rs
+
+/home/vscode/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/color_quant-1.1.0/src/lib.rs:
+/home/vscode/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/color_quant-1.1.0/src/math.rs:
