d71042e48a414df476288ba394f80d726d3bfd2e
mikedad <mikeiii.email@gmail.com>
2026-02-13 04:53:46 +0000

    Splash Screen Development

---
+++ target/release/deps/macroquad_macro-fea373de4288f85c.d
@@ -0,0 +1,5 @@
+/worktrees/testrepo/finn/target/release/deps/macroquad_macro-fea373de4288f85c.d: /home/vscode/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/macroquad_macro-0.1.8/src/lib.rs
+
+/worktrees/testrepo/finn/target/release/deps/libmacroquad_macro-fea373de4288f85c.so: /home/vscode/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/macroquad_macro-0.1.8/src/lib.rs
+
+/home/vscode/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/macroquad_macro-0.1.8/src/lib.rs:
---
+++ target/wasm32-unknown-unknown/release/deps/quad_rand-1635129deeb0c496.d
@@ -0,0 +1,8 @@
+/worktrees/testrepo/finn/target/wasm32-unknown-unknown/release/deps/quad_rand-1635129deeb0c496.d: /home/vscode/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/quad-rand-0.2.3/src/lib.rs /home/vscode/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/quad-rand-0.2.3/src/fy.rs
+
+/worktrees/testrepo/finn/target/wasm32-unknown-unknown/release/deps/libquad_rand-1635129deeb0c496.rlib: /home/vscode/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/quad-rand-0.2.3/src/lib.rs /home/vscode/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/quad-rand-0.2.3/src/fy.rs
+
+/worktrees/testrepo/finn/target/wasm32-unknown-unknown/release/deps/libquad_rand-1635129deeb0c496.rmeta: /home/vscode/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/quad-rand-0.2.3/src/lib.rs /home/vscode/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/quad-rand-0.2.3/src/fy.rs
+
+/home/vscode/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/quad-rand-0.2.3/src/lib.rs:
+/home/vscode/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/quad-rand-0.2.3/src/fy.rs:
