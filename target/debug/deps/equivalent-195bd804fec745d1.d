c8c15e8cd227b5a5f36279e64ed2999de99d9b87
mikedad <mikeiii.email@gmail.com>
2026-02-13 05:02:34 +0000

    Writing Tests

---
+++ target/debug/deps/equivalent-195bd804fec745d1.d
@@ -0,0 +1,7 @@
+/worktrees/testrepo/finn/target/debug/deps/equivalent-195bd804fec745d1.d: /home/vscode/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/equivalent-1.0.2/src/lib.rs
+
+/worktrees/testrepo/finn/target/debug/deps/libequivalent-195bd804fec745d1.rlib: /home/vscode/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/equivalent-1.0.2/src/lib.rs
+
+/worktrees/testrepo/finn/target/debug/deps/libequivalent-195bd804fec745d1.rmeta: /home/vscode/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/equivalent-1.0.2/src/lib.rs
+
+/home/vscode/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/equivalent-1.0.2/src/lib.rs:
---
+++ target/debug/deps/foldhash-b60a8579d17cfec5.d
@@ -0,0 +1,10 @@
+/worktrees/testrepo/finn/target/debug/deps/foldhash-b60a8579d17cfec5.d: /home/vscode/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/foldhash-0.1.5/src/lib.rs /home/vscode/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/foldhash-0.1.5/src/fast.rs /home/vscode/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/foldhash-0.1.5/src/quality.rs /home/vscode/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/foldhash-0.1.5/src/seed.rs
+
+/worktrees/testrepo/finn/target/debug/deps/libfoldhash-b60a8579d17cfec5.rlib: /home/vscode/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/foldhash-0.1.5/src/lib.rs /home/vscode/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/foldhash-0.1.5/src/fast.rs /home/vscode/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/foldhash-0.1.5/src/quality.rs /home/vscode/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/foldhash-0.1.5/src/seed.rs
+
+/worktrees/testrepo/finn/target/debug/deps/libfoldhash-b60a8579d17cfec5.rmeta: /home/vscode/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/foldhash-0.1.5/src/lib.rs /home/vscode/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/foldhash-0.1.5/src/fast.rs /home/vscode/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/foldhash-0.1.5/src/quality.rs /home/vscode/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/foldhash-0.1.5/src/seed.rs
+
+/home/vscode/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/foldhash-0.1.5/src/lib.rs:
+/home/vscode/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/foldhash-0.1.5/src/fast.rs:
+/home/vscode/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/foldhash-0.1.5/src/quality.rs:
+/home/vscode/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/foldhash-0.1.5/src/seed.rs:
