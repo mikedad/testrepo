b69f8f585b655be5ad90ce0e8894b8115b8c3234
mikedad <mikeiii.email@gmail.com>
2026-02-13 06:00:32 +0000

    Build timestamp display

---
+++ target/debug/build/dungeon_creator-c62b68b2b211c0fa/build_script_build-c62b68b2b211c0fa.d
@@ -0,0 +1,5 @@
+/worktrees/testrepo/finn/target/debug/build/dungeon_creator-c62b68b2b211c0fa/build_script_build-c62b68b2b211c0fa.d: build.rs
+
+/worktrees/testrepo/finn/target/debug/build/dungeon_creator-c62b68b2b211c0fa/build_script_build-c62b68b2b211c0fa: build.rs
+
+build.rs:
---
+++ target/debug/deps/dungeon_creator-b76c1331bc51e21d.d
@@ -0,0 +1,10 @@
+/worktrees/testrepo/finn/target/debug/deps/dungeon_creator-b76c1331bc51e21d.d: src/main.rs src/audio.rs src/song.rs src/splash.rs
+
+/worktrees/testrepo/finn/target/debug/deps/dungeon_creator-b76c1331bc51e21d: src/main.rs src/audio.rs src/song.rs src/splash.rs
+
+src/main.rs:
+src/audio.rs:
+src/song.rs:
+src/splash.rs:
+
+# env-dep:BUILD_TIMESTAMP=2026-02-13 06:00:29 UTC
