eba48d9d18570b3dce1ca3a4fda4a194fac2c068
mikedad <mikeiii.email@gmail.com>
2026-02-13 05:43:01 +0000

    Code Implementation

---
+++ target/debug/deps/alsa_sys-d849a57a0db0a521.d
@@ -0,0 +1,8 @@
+/worktrees/testrepo/finn/target/debug/deps/alsa_sys-d849a57a0db0a521.d: /home/vscode/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/alsa-sys-0.3.1/src/lib.rs /home/vscode/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/alsa-sys-0.3.1/src/generated.rs
+
+/worktrees/testrepo/finn/target/debug/deps/libalsa_sys-d849a57a0db0a521.rlib: /home/vscode/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/alsa-sys-0.3.1/src/lib.rs /home/vscode/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/alsa-sys-0.3.1/src/generated.rs
+
+/worktrees/testrepo/finn/target/debug/deps/libalsa_sys-d849a57a0db0a521.rmeta: /home/vscode/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/alsa-sys-0.3.1/src/lib.rs /home/vscode/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/alsa-sys-0.3.1/src/generated.rs
+
+/home/vscode/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/alsa-sys-0.3.1/src/lib.rs:
+/home/vscode/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/alsa-sys-0.3.1/src/generated.rs:
---
+++ target/debug/deps/dungeon_creator-e1780b9168e7a528.d
@@ -0,0 +1,8 @@
+/worktrees/testrepo/finn/target/debug/deps/dungeon_creator-e1780b9168e7a528.d: src/main.rs src/audio.rs src/song.rs src/splash.rs
+
+/worktrees/testrepo/finn/target/debug/deps/dungeon_creator-e1780b9168e7a528: src/main.rs src/audio.rs src/song.rs src/splash.rs
+
+src/main.rs:
+src/audio.rs:
+src/song.rs:
+src/splash.rs:
