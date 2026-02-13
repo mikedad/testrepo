344b0d5ad24a8237fc9d51e8b6ee2a4f3f73c840
mikedad <mikeiii.email@gmail.com>
2026-02-13 05:02:24 +0000

    ⠐ Writing Tests

---
+++ target/debug/deps/adler2-812557bf3732267a.d
@@ -0,0 +1,8 @@
+/worktrees/testrepo/finn/target/debug/deps/adler2-812557bf3732267a.d: /home/vscode/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/adler2-2.0.1/src/lib.rs /home/vscode/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/adler2-2.0.1/src/algo.rs
+
+/worktrees/testrepo/finn/target/debug/deps/libadler2-812557bf3732267a.rlib: /home/vscode/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/adler2-2.0.1/src/lib.rs /home/vscode/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/adler2-2.0.1/src/algo.rs
+
+/worktrees/testrepo/finn/target/debug/deps/libadler2-812557bf3732267a.rmeta: /home/vscode/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/adler2-2.0.1/src/lib.rs /home/vscode/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/adler2-2.0.1/src/algo.rs
+
+/home/vscode/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/adler2-2.0.1/src/lib.rs:
+/home/vscode/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/adler2-2.0.1/src/algo.rs:
