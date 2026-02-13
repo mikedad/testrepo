1e9abbe4b61f00fe9c40c8eb4a97e7d0a707e4d2
mikedad <mikeiii.email@gmail.com>
2026-02-13 05:02:51 +0000

    ⠐ Writing Tests

---
+++ target/debug/deps/macroquad_macro-3eb101a8b0ffe200.d
@@ -0,0 +1,5 @@
+/worktrees/testrepo/finn/target/debug/deps/macroquad_macro-3eb101a8b0ffe200.d: /home/vscode/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/macroquad_macro-0.1.8/src/lib.rs
+
+/worktrees/testrepo/finn/target/debug/deps/libmacroquad_macro-3eb101a8b0ffe200.so: /home/vscode/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/macroquad_macro-0.1.8/src/lib.rs
+
+/home/vscode/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/macroquad_macro-0.1.8/src/lib.rs:
---
+++ target/debug/deps/quad_rand-3a0d87f9222fbabe.d
@@ -0,0 +1,8 @@
+/worktrees/testrepo/finn/target/debug/deps/quad_rand-3a0d87f9222fbabe.d: /home/vscode/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/quad-rand-0.2.3/src/lib.rs /home/vscode/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/quad-rand-0.2.3/src/fy.rs
+
+/worktrees/testrepo/finn/target/debug/deps/libquad_rand-3a0d87f9222fbabe.rlib: /home/vscode/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/quad-rand-0.2.3/src/lib.rs /home/vscode/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/quad-rand-0.2.3/src/fy.rs
+
+/worktrees/testrepo/finn/target/debug/deps/libquad_rand-3a0d87f9222fbabe.rmeta: /home/vscode/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/quad-rand-0.2.3/src/lib.rs /home/vscode/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/quad-rand-0.2.3/src/fy.rs
+
+/home/vscode/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/quad-rand-0.2.3/src/lib.rs:
+/home/vscode/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/quad-rand-0.2.3/src/fy.rs:
